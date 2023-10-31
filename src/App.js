d12f6e86ad338e022f2f70eee73694080bd766f7
Grillied <eandersin95ram@gmail.com>
2023-10-30 21:50:16 -0400

    2nd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
+import Navbar from './Components/Navbar';
+import Cards from './Components/Cards';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
      <h1>CS 230L</h1>
      <h2>Section - 02</h2>
      <p>WVU ID: 800397051</p>
      <p>Hi, I am Evan Anderson.</p>
+     <Cards></Cards>
+    
     </div>
   );
 }
